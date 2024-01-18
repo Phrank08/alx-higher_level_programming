8501452dbd5437cf75e5552c1247ee48155f5219
Phrank08 <frankokey469@gmail.com>
2024-01-18 11:59:18 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for y in range(10):
     for x in range(10):
-        print("f{y}{x}", end=', ')
+        print("{:d}{:d}".format(y,x), end=', ')
         if y == 9 and x == 9:
-            print("f{y}{x}", end='')
+            print("{:d}{:d}".format(y,x), end='')
