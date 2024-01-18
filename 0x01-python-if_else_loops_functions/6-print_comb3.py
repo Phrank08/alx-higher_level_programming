84584779a643786f784e691b3f000ea66605371c
Phrank08 <frankokey469@gmail.com>
2024-01-18 13:00:47 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for y in range(9):
-    for x in range(10):
+    for x in range(y + 1,10):
         print("{:d}{:d}".format(y, x), end=', ')
         if y == 8 and x == 9:
             print("{:d}{:d}".format(y, x), end='')
