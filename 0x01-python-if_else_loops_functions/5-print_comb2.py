31be4352110324bb413a9d43a0341cb0827a27b2
Phrank08 <frankokey469@gmail.com>
2024-01-18 12:14:24 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for y in range(0, 99)
-    print("{:d}".format(y),end='')
-        if y < 99:
+for y in range(0, 99):
+     print("{:d}".format(y), end='')
+     if y < 99:
             else:
                 print("\n")
