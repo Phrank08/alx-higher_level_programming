85e87b3eeca5cb18852d20680c88f631a32073ef
Phrank08 <frankokey469@gmail.com>
2024-01-18 12:10:16 -0800

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for y in range(10):
-    for x in range(10):
-        print("{:d}{:d}".format(y,x), end=', ')
-        if y == 9 and x == 9:
-            print("{:d}{:d}".format(y,x), end='')
+for y in range(0, 99)
+    print("{:d}".format(y),end='')
+        if y < 99:
+            else:
+                print("\n")
