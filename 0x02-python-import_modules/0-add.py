cc4d6455ce2633bda74572a041e2f9afa670d18c
Phrank08 <frankokey469@gmail.com>
2024-01-22 18:51:23 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 a = 1
 b = 2
+
 add = add_0(1, 2)
-print("{:d}" + "{:d}" = "{:d}".format(a, b, add))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
