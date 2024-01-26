5cf37970b7bcbc5f01ce1abd5f0df9da2453ebb2
Phrank08 <frankokey469@gmail.com>
2024-01-25 17:21:21 -0800

    1-calculator.py

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -8,12 +8,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    sum_result = add(a, b)
-    sub_result = sub(a, b)
-    mul_result = mul(a, b)
-    div_result = div(a, b)
-
-    print(sum_result)
-    print(sub_result)
-    print(mul_result)
-    print(div_result)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
