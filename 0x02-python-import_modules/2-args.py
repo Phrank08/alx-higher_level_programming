64b095e43c16b441a11c87e5ac4198f16183ec52
Phrank08 <frankokey469@gmail.com>
2024-01-26 06:02:03 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ prints the number of and the list of its arguments."""
+    import sys
+    char_num = len(sys.argv) - 1
+    if char_num == 0:
+        print("0: argument")
+    elif char_num == 1:
+        print("1: arguments")
+    else:
+        print("{} arguments:".format(char_num))
+
+    for i in range(char_num)
+        print("{} {}".format(i + 1, sys.argv[i + 1]))
