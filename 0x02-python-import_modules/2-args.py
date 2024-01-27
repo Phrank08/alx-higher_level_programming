08ecd978aa8337860a792b17d8b843ee22180371
Phrank08 <frankokey469@gmail.com>
2024-01-27 13:53:46 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
 
     char_num = len(sys.argv) - 1
     if char_num == 0:
-        print("0 argument")
+        print("0 arguments.")
     elif char_num == 1:
-        print("1 arguments")
+        print("1 argument:")
     else:
         print("{} arguments:".format(char_num))
     for i in range(char_num):
-        print("{} {}".format(i + 1, sys.argv[i + 1]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+~                                                           
