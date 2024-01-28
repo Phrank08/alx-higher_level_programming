c22d96a4103f3bfde9c44bab56a1c3b9e7ee2e1d
Phrank08 <frankokey469@gmail.com>
2024-01-27 17:29:44 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,8 @@
 if __name__ == "__main__":
     """prints all the names defined by the compiled module hidden_4.pyc"""
     import hidden_4
+
     x = dir(hidden_4)
     for i in range x:
         if x[:2] != "__":
-            print(x)
+            print(i)
