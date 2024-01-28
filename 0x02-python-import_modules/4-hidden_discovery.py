428671575745b1b3565e1e74cc85190d4bffc4ba
Phrank08 <frankokey469@gmail.com>
2024-01-27 17:39:57 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     x = dir(hidden_4)
-    for i in range x:
-        if x[:2] != "__":
+    for i in x:
+        if i[:2] != "__":
             print(i)
