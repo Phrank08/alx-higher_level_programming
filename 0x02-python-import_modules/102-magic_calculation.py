b67abf7ae796f743a6544a6513b079674dfc7fc5
Phrank08 <frankokey469@gmail.com>
2024-01-31 11:11:05 -0800

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     """Write Python function to match Holberton School provided bytecode."""
-    result = 0
-    operations = ('add', 'sub')
-
     from magic_calculation_102 import add, sub
 
-    add_func = add
-    sub_func = sub
-
     if a < b:
-        result = add_func(a, b)
-
+        operations = add(a, b)
         for i in range(4, 6):
-            result = add_func(result, i)
+            operations = add(operations, i)
+        return (operations)
 
     else:
-        result = sub_func(a, b)
-
-    return result
-
+        return(sub(a, b))
