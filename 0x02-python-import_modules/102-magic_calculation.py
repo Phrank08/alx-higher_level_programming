1bc282f3985ceeb5eed6f3b76aef46b6e0b16438
Phrank08 <frankokey469@gmail.com>
2024-01-31 10:57:51 -0800

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    result = 0
+    operations = ('add', 'sub')
+
+    from magic_calculation_102 import add, sub
+
+    add_func = add
+    sub_func = sub
+
+    if a < b:
+        result = add_func(a, b)
+
+        for i in range(4, 6):
+            result = add_func(result, i)
+
+    else:
+        result = sub_func(a, b)
+
+    return result
+
