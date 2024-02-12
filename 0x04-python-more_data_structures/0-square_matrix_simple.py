b4ba3d4fe7d0d2b244daa365605f4ff7a1410eb5
Phrank08 <frankokey469@gmail.com>
2024-02-11 16:44:09 -0800

    0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,13 +2,13 @@
 def square_matrix_simple(matrix=[]):
     """ A function that computes the square value of all integers of a matrix"""
     if matrix is not None:
-    new_matrix = [] 
+        new_matrix = [] 
     for row in matrix:
         if row is not None:
             new_row = []
             for element in row:    
                 new_row.append(element ** 2)
-        new_matrix.append(new_row)
+            new_matrix.append(new_row)
         return new_matrix
     else:
         return matrix
