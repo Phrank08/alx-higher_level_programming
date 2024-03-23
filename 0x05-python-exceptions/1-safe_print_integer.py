b56b94a78b7696bc44b64a37499464e91986bc6e
Phrank08 <frankokey469@gmail.com>
2024-03-23 16:58:39 -0700

    errors and exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(valueAsInt))
         return True
-    except Exception as e:
+    except (ValueError, TypeError):
         return False
-    finally:
-        print()
+    else:
+        return True
