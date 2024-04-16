00ef9ec0a21554d3e94afb2da0c89c8734ad9090
Phrank08 <frankokey469@gmail.com>
2024-04-16 12:28:21 -0700

    Classes And Objects In Python

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
-Class Square:
+"""
+This program defines an empty class Square
+"""
+class Square:
+    """
+    Thhis is a class that does nothing but defines a class
+    """
     pass
