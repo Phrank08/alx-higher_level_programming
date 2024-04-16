159faf90bea0812ce766a6193d235f15f3c07b62
Phrank08 <frankokey469@gmail.com>
2024-04-16 13:58:20 -0700

    Classes And Objects in Python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a class named square with private instance"""
+
+
+class Square:
+    """Contains a private instance attribute"""
+    def __init___(self, size):
+        """A Function that initializes size"""
+        self.size = size
