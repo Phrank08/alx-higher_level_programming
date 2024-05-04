91b153f4833fdcb452bbe9b03a00ab5f9e553f9f
Phrank08 <frankokey469@gmail.com>
2024-05-03 21:16:14 -0700

    Outputs file to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,7 +7,7 @@ def read_file(filename=""):
     """A function that reads a text file (UTF8) 
     and prints it to stdout
     """
-    with open("filename", encoding "UTF=8") as myFile:
+    with open(filename) as myFile:
         TextContainer = myFile.read()
         print(TextContainer, end="")
 
