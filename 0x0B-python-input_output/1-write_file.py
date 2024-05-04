edaa93a6d1d9debc2b6e7a0924d2cec14dba957d
Phrank08 <frankokey469@gmail.com>
2024-05-03 21:29:01 -0700

    returns number of characters

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    A module that returns number of characters written
+
+"""
+
+def write_file(filename="", text=""):
+    """
+    A function that writes a string to a text file
+    """
+
+    with open(filename, "w") as myFile:
+        NumCharacters = f.write(text)
+
+        return NumCharacters
+
