e3eb212f0e1617f5a49fc2563f0d1694a1f4124b
Phrank08 <frankokey469@gmail.com>
2024-05-03 21:53:19 -0700

    json representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+JSON representation module
+"""
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object
+    """
+
+    return json.dumps(my_obj)
