127c080936b5c0da3dc4bd05670f344a282138f2
Phrank08 <frankokey469@gmail.com>
2024-05-11 17:50:55 -0700

    add:shapers

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,18 +4,7 @@
 A class module
 """
 
-class Base:
-    """
-    A class named named Base
-    """
-    __nb_objects = 0
-    def __init__(self, id=None):
-
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+from .base import Base
 
 class Rectangle(Base):
     """
