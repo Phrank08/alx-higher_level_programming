79b6e3e4576c5b3495fcbdf48db0d1fc0ab1f98a
Phrank08 <frankokey469@gmail.com>
2024-05-22 18:51:55 -0700

    creates database only if it does not exist

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- this script creates a database
 -- in a server. If the database already exists, script should not fail.
-CREATE DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
