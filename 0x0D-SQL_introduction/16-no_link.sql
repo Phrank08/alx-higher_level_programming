29780ac73a7236b81a459949d4512b1a808adb7a
Phrank08 <frankokey469@gmail.com>
2024-05-25 12:29:15 -0700

    a script that lists all records of a table.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- a script that lists all records of a table.
-SELECT 'score', 'name'
-FROM 'econd_table'
-WHERE 'name' IS NOT NULL
-ORDER BY DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` IS NOT NULL
+ORDER BY `score` DESC;
