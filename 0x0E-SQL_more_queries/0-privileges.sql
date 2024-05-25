f25622d66631d7f3a6142294929f762db6919b10
Phrank08 <frankokey469@gmail.com>
2024-05-25 13:58:15 -0700

    list all privileges of user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- List privileges for user_0d_1
-SHOW GRANTS FOR user_0d_1@localhost;
--- List privileges for user_0d_2
-SHOW GRANTS FOR user_0d_2@localhost;
+-- list all privileges of user
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
