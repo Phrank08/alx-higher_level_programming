5585ab7d651225078a70924e513af39390a748ea
Phrank08 <frankokey469@gmail.com>
2024-06-12 13:39:08 -0700

    Add: a script that checks if the first argument can be converted to a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-let myVar;
 const strInt = parseInt(process.argv[2]);
 if (isNaN(strInt)) {
-  myVar = 'Not a number';
+  console.log('Not a number');
 } else {
-  myVar = 'My number: ' + strInt;
+  console.log('My number: ' + strInt);
 }
-console.log(myVar);
+// console.log(myVar);
