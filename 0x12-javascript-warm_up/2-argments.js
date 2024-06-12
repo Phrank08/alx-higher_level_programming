4c5c806bafc4322d74cda002a981be21dc6d0765
Phrank08 <frankokey469@gmail.com>
2024-06-12 12:42:15 -0700

    Add: js script that prints message depending on arg(s) passed

---
+++ 0x12-javascript-warm_up/2-argments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.argv.length <= 2){
+	console.log('No argument');
+} else if (process.argv.length === 3){
+	console.log('Aruments found');
+} else {
+	console.log('Arguments found');
+}
+
