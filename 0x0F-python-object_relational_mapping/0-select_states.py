80084987d571cec229c4f031a3a87cc2aadac51a
Phrank08 <frankokey469@gmail.com>
2024-06-27 14:00:17 -0700

    List all states from database hbnt_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa:
+"""
+
+import MySQLdb
+import sys
+
+def main():
+
+    HOST = 'localhost'
+    PORT = 3306
+    USER = sys.argv[1]
+    PASS = sys.argv[2]
+    DB = sys.argv[3]
+
+    database = MySQLdb.connect(
+            host = HOST,
+            port = PORT,
+            user USER,
+            passwd = PASS,
+            database = DB
+    )
+    cur = database.connect()
+    cur.execute("SELECT id, name, FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    database.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+ORM is a programming technique used to convert data between incompatible type systems using object-oriented programming languages.
