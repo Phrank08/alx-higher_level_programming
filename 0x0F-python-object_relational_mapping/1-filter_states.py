a22d3449a5703b404fdeed92f8f29fc18b0fc01b
Phrank08 <frankokey469@gmail.com>
2024-06-28 17:08:26 -0700

    Task Completed

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,39 +1,23 @@
 #!/usr/bin/python3
-
-""" lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-"""
+"""my module on MySQLdb"""
 import MySQLdb
-from sys import argv
-
-def main():
-    """
-        lists all states beginning with letter 'N' in ascending order
-    """
-    My_Host = 'localhost'
-    My_Port = 3306
-    My_User = sys.argv[1]
-    My_Passwd = sys.argv[2]
-    My_db = sys.argv[3]
-
-    db = MySQLdb.connect(
-            HOST = My_Host,
-            PORT = My_Port,
-            USER = My_User,
-            PASSWD = My_Passwd,
-            db = My_db
-    )
-
-    MyCur = db.cursor()
-    first_query = "SELECT id, name FROM states WHERE name"
-    second_query = "LIKE BINARY 'N%' ORDER BY id ASC"
-    final_query = first_query + second_query
-    Mycur.execute(final_query)
-    rows = Mycur.fetchall()
-    for row in rows:
-        print(row)
-    MyCur.close()
-    db.close()
-
-
+import sys
 if __name__ == "__main__":
-    main()
+    args = sys.argv
+    user = args[1]
+    password = args[2]
+    database = args[3]
+
+    db = MySQLdb.connect(host='localhost', user=user,
+                         password=password, port=3306, database=database)
+
+    cur = db.cursor()
+    query = """SELECT * FROM states
+            WHERE BINARY name LIKE 'N%'"""
+    cur.execute(query)
+    result = cur.fetchall()
+    for res in result:
+        print(res)
+
+    cur.close()
+    db.close()
