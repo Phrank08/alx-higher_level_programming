93a79b59f344480d9592ab5c9bd331e531e8b23f
Phrank08 <frankokey469@gmail.com>
2024-06-28 17:16:15 -0700

    Task Done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/biin/python3
-    """ a script that takes in an argument and displays all values in the states that matches the argument.
+    """ a script that takes in an argument and displays all values in the states t          able of hbtn_0e_0_usa where name matches the argument.
     """
 
 import MySQLdb
