c4c5885dd8b305c28bc05d3281d7f98d404e20c7
Phrank08 <frankokey469@gmail.com>
2024-07-08 13:30:58 -0700

    Task Done

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sortedDictionary = sorted(a_dictionary)
-
-    for keys in sortedDictionary:
-        print(f"{keys}: {sortedDictionary}")
-    return sortedDictionary
+    for item, values in sorted(a_dictionary.items()):
+        print(f"{item}: {values}")
