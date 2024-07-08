9c31d421fe2a36b90dd03e676b122390ef0aebfd
Phrank08 <frankokey469@gmail.com>
2024-07-08 14:30:11 -0700

    Task Done

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,28 +1,16 @@
 #!/usr/bin/python3
 """
-Text Indentation Module
+    This Module That Indents a string
+    Args:
+        text(str): string to be indented
 """
-
-
 def text_indentation(text):
     """
-    Text Indentation Method.
+        This Function indent a string when it comes in contact with . ? :
     """
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    lenght = len(text)
-    i = 0
-    while (i < lenght):
-        # if text[i] == '.' or text[i] == '?' or text[i] == ':':
-        if text[i] in (".", "?", ":"):
-            if (i + 1) != lenght:
-                s = text[i + 1]
-                if (s != '.' and s != '?' and s != ':'):
-                    print(text[i], end='\n\n')
-                    if s == ' ':
-                        i += 2
-                    if s != ' ':
-                        i += 1
-        else:
-            print(text[i], end='')
-            i += 1
+    for char in text:
+        print(char,end="")
+        if char in [".", "?", ":"]:
+            print("\n\n", end="")
