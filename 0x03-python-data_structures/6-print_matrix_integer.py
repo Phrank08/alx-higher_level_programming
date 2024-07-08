f3506941545f4387a3a5bb14d2a72f769cf63a21
Phrank08 <frankokey469@gmail.com>
2024-07-08 12:44:20 -0700

    Task Done

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""a function that prints a matrix of integers."""
 def print_matrix_integer(matrix=[[]]):
-    if matrix is not None:
-        x = 0
-        for horizon in matrix:
-            y = 0
-            for element in horizon:
-                print("{:d} ".format(matrix[x][y]), end="")
-                y += 1
-            print()
-            x += 1
+    for i in matrix:
+        for j in i:
+            if j == i[-1]:
+                print("{:d}".format(j), end="")
+            else:
+                print("{:d}".format(j), end=" ")
+        print()
