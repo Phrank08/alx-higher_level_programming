cf47caa48b55a3268c7595eb30912ca75baf12e4
Phrank08 <frankokey469@gmail.com>
2024-07-08 13:38:19 -0700

    Task Done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary  == {}:
-        return None
-    elif a_dictionary is not None:
-        sortScore = sorted(a_dictionary.value())
-        i = sortScore[len(sortScore) - 1]
-
-        for key, digit in a_dictionary.items():
-            if digit == i:
+    if a_dictionary:
+        highest = max(a_dictionary.values())
+        for key, value in a_dictionary.items():
+            if value == highest:
                 return key
-            else:
-                return None
