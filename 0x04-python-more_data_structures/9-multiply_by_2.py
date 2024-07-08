51456762411086c70add74c6cab53e180e7cbdb9
Phrank08 <frankokey469@gmail.com>
2024-07-08 13:37:20 -0700

    Task Done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dictionary = {}
-    for keys in a_dictionary:
-        new_dictionary[key] = a_dictionary[key] * 2
-    return new_dictionary
+    new_dic = {}
+    for keys, value in sorted(a_dictionary.items()):
+        new_dic[keys] = value * 2
+    return new_dic
