7355b7a436cecd6281d300164343ae140a0b352e
Phrank08 <frankokey469@gmail.com>
2024-07-08 12:38:40 -0700

    Task Done

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-"""A function that prints all integers of a lists"""
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+    for i in my_list:
+        print("{:d}".format(i))
