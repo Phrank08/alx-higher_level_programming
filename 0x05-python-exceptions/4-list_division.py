118438d48b76630a9f70549ffcd6535915b7d5bb
Phrank08 <frankokey469@gmail.com>
2024-07-08 14:02:04 -0700

    Task Done

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for i in range(list_length):
-            try:
-                value = my_list_1[i] / my_list_2[i]
-            except TypeErrror:
-                print("wrong type")
-                new_list.append(0)
-            except ZeroDivisionError:
-                print("division by 0")
-                new_list.append(0)
-            except IndexError:
-                print("out of range")
-                new_list.append(0)
-            else:
-                new_list.append(value)
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except (TypeError, ValueError):
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
         finally:
-            return new_list
+            new_list.append(result)
+    return new_list
+
+# you can use this fot test case
+# my_l_1 = [10, 8, 4, 4]
+# my_l_2 = [2, 0, "H", 2, 7]
+# result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
+# print(result)
