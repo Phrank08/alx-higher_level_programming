63bad4d0e629a27e842551ab7798dc7852c86f64
Phrank08 <frankokey469@gmail.com>
2024-07-08 13:28:46 -0700

    Task Done

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-""" A function that returns a set of all elements in present in only one set"""
 def only_diff_elements(set_1, set_2):
-    updatedUnion = set_1.union(set_2)
-
-    return updatedUnion
+    set_union = set_1.symmetric_difference(set_2)
+    return set_union
