0976957b20d456ff4c95ec15838895f14d448111
Phrank08 <frankokey469@gmail.com>
2024-07-08 13:20:24 -0700

    Task Done

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if my_list is not None:
-        i = 0
-        NewList = []
-        for element in my_list:
-            if my_list[i] % 2 == 0:
-                NewList.append(True)
-            else:
-                new_list.append(False)
-            i = i + 1
-        return NewList
+
+    new_list = []
+
+    for num in my_list:
+
+        divisible_2 = num % 2 == 0
+
+        new_list.append(divisible_2)
+
+    return new_list
--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""a function that deletes the item at a specific position in a list."""
 def delete_at(my_list=[], idx=0):
-    NewIndex = my_list[idx]
-    if idx < 0:
+    if idx < 0 or idx > (len(my_list) - 1):
         return my_list
-    elif idx > len(my_list):
-        return my_list
-    else:
-        del NewIndex
-        return NewIndex
+    for i, index in enumerate(my_list):
+        if i == idx:
+            my_list.remove(my_list[i])
+    return my_list
