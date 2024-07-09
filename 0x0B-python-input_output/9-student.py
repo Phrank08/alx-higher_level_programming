f94255b0901fadcc95c57b042da4eb36177b2643
Phrank08 <frankokey469@gmail.com>
2024-07-08 18:36:38 -0700

    Done

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+""" A Module Student"""
 
-"""
-a class module
-"""
 
 class Student:
-    """
-    A class that defines a student
-    """
+    """A class for student"""
     def __init__(self, first_name, last_name, age):
+        """intilize new variables"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            return self.__dict__
+    def to_json(self):
+        """Return a dictionsry Representation"""
+        dict_obj = self.__dict__
+        for key, value in dict_obj.items():
+            if isinstance(value, (int, bool, str, dict, list)):
+                dict_obj[key] = value
+            elif isinstance(value, (tuple, list)):
+                dict_obj[key] = list(value)
+            elif hasattr(value, '__dict__'):
+                dict_obj = self.to_json(value)
+            
+        return dict_obj
