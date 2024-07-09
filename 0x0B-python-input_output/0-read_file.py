d3c989485172ab26777ba5956628d0e89c9e2371
Phrank08 <frankokey469@gmail.com>
2024-07-08 18:30:57 -0700

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-A module that reads file to the standard output
-"""
+"""A module that that reads a text file (UTF8) and prints it to stdout:"""
+
 
 def read_file(filename=""):
-    """A function that reads a text file (UTF8) 
-    and prints it to stdout
-    """
-    with open(filename) as myFile:
-        TextContainer = myFile.read()
-        print(TextContainer, end="")
-
+    """Function That reads a txt file"""
+    with open(filename, 'r', encoding="UTF8") as read_file:
+        data = read_file.read()
+        print(data, end="")
