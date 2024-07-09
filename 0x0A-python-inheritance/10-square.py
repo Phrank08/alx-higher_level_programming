1933a204eb64dad9b1ff84007cf00ec029149b37
Phrank08 <frankokey469@gmail.com>
2024-07-08 18:27:53 -0700

    Done

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,52 +1,11 @@
 #!/usr/bin/python3
-"""
-A module of a class
-"""
-
-
-class BaseGeometry:
-    """Geometry class
-    """
-    def area(self, width, height):
-        """
-        check area"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates value
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """inherits from BaseGeometry
-    """
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, "width", self.__width)
-        BaseGeometry.integer_validator(self, "height", self.__height)
-
-    def area(self):
-        """Method to calculate area of Rectangle
-        """
-        return self.__width * self.__height
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
+"""This Module is based on Based Geometry"""
+Rectangle = __import__("9-rectangle").Rectangle
 
 class Square(Rectangle):
-    """
-    A child class of Rectangle
-    """
+    """Square class inherits from a subclass Rectangle"""
     def __init__(self, size):
-        width = size
-        height = size
-        Rectangle.integer_validator(self, "size", size)
-        super().__init__(width, height)
-
+        """intilizing Square and intilizing the methods of Rectangle"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
